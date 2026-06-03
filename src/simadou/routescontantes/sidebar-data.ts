867adfbc17c0afca9f885ddc273c05d81e1e4e61
DiMalle <boubacarmalle86@gmail.com>
2026-06-03 22:40:32 +0000

    feat(politique): implement programme, cadre analytique, and indicateurs CMR pages.

--- src/simadou/routescontantes/sidebar-data.ts
+++ src/simadou/routescontantes/sidebar-data.ts
@@ -151,16 +151,6 @@ export const sidebarData: SidebarData = {
               url: '/programme/indicateurs-cmr',
               icon: TrendingUp,
             },
-            {
-              title: 'Cadre stratégique',
-              url: '/programme/cadre-strategique',
-              icon: Target,
-            },
-            {
-              title: 'Indicateurs stratégiques',
-              url: '/programme/indicateurs-performance',
-              icon: LineChart,
-            },
           ],
         },
 
