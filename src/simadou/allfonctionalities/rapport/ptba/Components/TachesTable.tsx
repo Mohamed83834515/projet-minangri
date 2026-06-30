355d0474a9a237de2b711d9d655333a68b592394
Amadou Bathily <amadouba7h@gmail.com>
2026-06-30 05:09:25 +0000

    refactor: integrate GenericTable component across export tables
    - Replaced manual table implementations in CoutActiviteTable, IndicateursTable, and TachesTable with the GenericTable component for improved consistency and maintainability.
    - Added custom row rendering logic to handle specific row types and enhance visual representation.
    - Cleaned up imports and optimized column definitions for better clarity and performance.

--- src/simadou/allfonctionalities/rapport/ptba/Components/TachesTable.tsx
+++ src/simadou/allfonctionalities/rapport/ptba/Components/TachesTable.tsx
@@ -296,7 +296,6 @@ export function TachesTable({
             search={{}}
             navigate={navigate}
             showPagination={false}
-            defaultPageSize={rows.length}
             showSearch={false}
             showViewOptions={false}
             customRowRenderer={(row, i, { rowClassName, cellClassName }) => {
@@ -305,7 +304,7 @@ export function TachesTable({
                 const spanColumns = columns.length - emptyColumns
 
                 return (
-                  <TableRow className={`${rowClassName} font-bold`} key={i}>
+                  <TableRow className={rowClassName} key={i}>
                     {Array.from({ length: emptyColumns }).map((_, index) => (
                       <TableCell key={index} className={cellClassName()} />
                     ))}
